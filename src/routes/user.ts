8d924a4260f1e8eb70cad24cf957e431b6a983ec
akturanilyas <akturanilyas@gmail.com>
2021-07-05 14:18:53 +0300

    Add: TODO line added.

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,12 +1,13 @@
+// TODO ADD this functions
 module.exports = ([
   {
     path: '/api/user/',
     method: 'post',
-    handler: (req, res, next) => registerUser(req, res, next),
+    handler: (req, res, next) => getUser(req, res, next),
   },
   {
     path: '/api/auth/login',
     method: 'post',
-    handler: (req, res, next) => loginUser(req, res, next),
+    handler: (req, res, next) => getAllUsers(req, res, next),
   },
 ]);
